bbffbc883621925c9cbd0846b5642f91683fac4a
creepersguitar <>
2024-01-03 10:26:11 +0000

    added consts into script and defined question and score numbers

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,15 @@
+// getters
+const question = document.getElementById("Question");
+const answer1 = document.getElementById("Answer1");
+const answer2 = document.getElementById("Answer2");
+const answer3 = document.getElementById("Answer3");
+const answer4 = document.getElementById("Answer4");
+const score = document.getElementById("score");
+const again = document.getElementById("play-Again");
+
+let QNumber = 0;
+let scoreNumber = 0
+
 function endOption(chosenOpt) {
 
 }
