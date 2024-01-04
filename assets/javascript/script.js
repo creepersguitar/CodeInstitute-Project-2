7016f59e9578763cc58db0e6c6a75e6a12e91f15
creepersguitar <>
2024-01-04 09:35:33 +0000

    added first question to script

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,6 +10,17 @@ const again = document.getElementById("play-Again");
 let QNumber = 0;
 let scoreNumber = 0
 
+const christmasQuestions = [
+    {
+        "question" : "What day is christmas day?",
+        "answers" : [
+            "December 25th", "December 1st", "January 26th", "December 29th", "May 5th"
+        ],
+        "correct" : 1
+    }
+]
+
+
 function endOption(chosenOpt) {
 
 }
