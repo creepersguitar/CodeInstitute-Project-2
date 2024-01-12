5477accf7908f99ad49d44796758b58bbe09abd8
George Small <gsrr201@gmail.com>
2024-01-12 12:25:07 +0000

    changed spelling on variables in both css and js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,10 +1,10 @@
 // getters
 const question = document.getElementById("question");
-const answer1 = document.getElementById("Answer1");
-const answer2 = document.getElementById("Answer2");
-const answer3 = document.getElementById("Answer3");
-const answer4 = document.getElementById("Answer4");
-const answer5 = document.getElementById("Answer5");
+const answer1 = document.getElementById("answerOne");
+const answer2 = document.getElementById("answerTwo");
+const answer3 = document.getElementById("answerThree");
+const answer4 = document.getElementById("answerFour");
+const answer5 = document.getElementById("answerFive");
 const score = document.getElementById("score");
 const again = document.getElementById("play-Again");
 
