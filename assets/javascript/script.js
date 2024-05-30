8837d82681c11e7087bc5d8e949a58b0212ee490
George Small <gsrr201@gmail.com>
2024-05-30 09:44:18 +0000

    fixed play again issues and green buttons issues

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,4 @@
-// getters (gets elements by their IDs)
+// Get elements by their IDs
 const question = document.getElementById("question");
 const answer1 = document.getElementById("answerOne");
 const answer2 = document.getElementById("answerTwo");
@@ -6,225 +6,230 @@ const answer3 = document.getElementById("answerThree");
 const answer4 = document.getElementById("answerFour");
 const answer5 = document.getElementById("answerFive");
 const score = document.getElementById("scoreNumber");
-const again = document.getElementById("play-Again");
-const heroText = document.getElementById("heroText");
+const heroText = document.getElementById("hero-text");
 const startquiz = document.getElementById("startQuizBtn");
-// Get the modal, button, and span elements
 const modal = document.getElementById("myModal");
-const btn = document.getElementById("myBtn");
 const span = document.getElementsByClassName("close")[0];
-// initialises variables
+
+// Initialize variables
 let qNumber = 0;
 let scoreNumber = 0;
+
 // List of questions
 const christmasQuestions = [
     {
-        "question" : "What day is Christmas day?",
-        "answers" : [
-            "December 1st", "December 25th", "January 26th", "December 29th", "May 5th"
-        ],
-        "correct" : 1
+        "question": "What day is Christmas day?",
+        "answers": ["December 1st", "December 25th", "January 26th", "December 29th", "May 5th"],
+        "correct": 1
     },
     {
-        "question" : "Which popular Christmas beverage is also called 'milk punch?'",
-        "answers" : [
-            "eggnog", "Juice", "rum", "Milkshake", "Milk"
-        ],
-        "correct" : 0
+        "question": "Which popular Christmas beverage is also called 'milk punch?'",
+        "answers": ["eggnog", "Juice", "rum", "Milkshake", "Milk"],
+        "correct": 0
     },
     {
-        "question" : "What did the other reindeer not let Rudolph do because of his shiny red nose?",
-        "answers" : [
-            "eat with them","sleep with them","drive the sleigh","have fun with them","Join in any reindeer games"
-        ],
-        "correct" : 4
+        "question": "What did the other reindeer not let Rudolph do because of his shiny red nose?",
+        "answers": ["eat with them", "sleep with them", "drive the sleigh", "have fun with them", "Join in any reindeer games"],
+        "correct": 4
     },
     {
-        "question" : "What are the two other most popular names for Santa Claus?",
-        "answers" : [
-            "Saint Kringle and rant single","Santa baby and claus kringle","Kris Kringle and Saint Nick","Saint George and nicholas klaus","Kris Klaus and santa claus"
-        ],
-        "correct" : 2
+        "question": "What are the two other most popular names for Santa Claus?",
+        "answers": ["Saint Kringle and rant single", "Santa baby and claus kringle", "Kris Kringle and Saint Nick", "Saint George and nicholas klaus", "Kris Klaus and santa claus"],
+        "correct": 2
     },
     {
-        "question" : "Which Hollywood actor played six different roles in The Polar Express?",
-        "answers" : [
-            "Eddie Deezen","Peter Scolari","michael jeter","Tom Hanks","Will smith"
-        ],
-        "correct" : 3
+        "question": "Which Hollywood actor played six different roles in The Polar Express?",
+        "answers": ["Eddie Deezen", "Peter Scolari", "michael jeter", "Tom Hanks", "Will smith"],
+        "correct": 3
     },
     {
-        "question" : "What is the name of the snowman in Frozen?",
-        "answers" : [
-            "olaf","olan","kyle","Peter","Will"
-        ],
-        "correct" : 0
+        "question": "What is the name of the snowman in Frozen?",
+        "answers": ["olaf", "olan", "kyle", "Peter", "Will"],
+        "correct": 0
     },
     {
-        "question" : "How many reindeer drive Santa's sleigh (including Rudolph)?",
-        "answers" : [
-            "1","6","8","9","10"
-        ],
-        "correct" : 3
+        "question": "How many reindeer drive Santa's sleigh (including Rudolph)?",
+        "answers": ["1", "6", "8", "9", "10"],
+        "correct": 3
     },
     {
-        "question" : "How many Home Alone films are there?",
-        "answers" : [
-            "9","1","3","45","6"
-        ],
-        "correct" : 4
+        "question": "How many Home Alone films are there?",
+        "answers": ["9", "1", "3", "45", "6"],
+        "correct": 4
     },
     {
-        "question" : "In which year was the first Christmas card sent?",
-        "answers" : [
-            "1843", "1900", "1438", "1239", "1066"
-        ],
-        "correct" : 1
+        "question": "In which year was the first Christmas card sent?",
+        "answers": ["1843", "1900", "1438", "1239", "1066"],
+        "correct": 0
     },
     {
-        "question" : "Which celebrity won the first ever Strictly Come Dancing in December 2004?",
-        "answers" : [
-            "Jamie borthwick and Nancy Xu","Debbie Mcgee and kevin clifton","Natasha Kaplinsky with Brendan Cole","Katie Durham and brendan Cole","Anne Marie and Graziano"
-        ],
-        "correct" : 2
+        "question": "Which celebrity won the first ever Strictly Come Dancing in December 2004?",
+        "answers": ["Jamie borthwick and Nancy Xu", "Debbie Mcgee and kevin clifton", "Natasha Kaplinsky with Brendan Cole", "Katie Durham and brendan Cole", "Anne Marie and Graziano"],
+        "correct": 2
     },
     {
-        "question" : "The Snowman was shown on TV for the first time on Boxing Day of which year?",
-        "answers" : [
-            "1289","1982","1298","1928","1829"
-        ],
-        "correct" : 1
+        "question": "The Snowman was shown on TV for the first time on Boxing Day of which year?",
+        "answers": ["1289", "1982", "1298", "1928", "1829"],
+        "correct": 1
     },
     {
-        "question" : "Which British monarch delivered the first ever Christmas message?",
-        "answers" : [
-            "King George IV","Queen Elizabeth I","King Charles","Queen Victoria","King George V"
-        ],
-        "correct" : 4
+        "question": "Which British monarch delivered the first ever Christmas message?",
+        "answers": ["King George IV", "Queen Elizabeth I", "King Charles", "Queen Victoria", "King George V"],
+        "correct": 4
     },
 ];
-// variable to store the number of questions
+
+// Variable to store the number of questions
 const qlength = christmasQuestions.length;
 
-
-// When clicked display modal
+// Close modal when the span (x) is clicked
 span.onclick = function () {
     modal.style.display = "none";
 };
+
+// Close modal when clicking outside of the modal
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 };
+
 /**
- * shows the modal
- */
-function showModel() {
-    modal.style.display = "visible";
-}
-/**
- * closes the modal
- */
-function closeModal() {
-    modal.style.display = "none";
-}
-/**
- * function to load the question to the page
+ * Function to load the question to the page
  */
 function loadGameQuestion(qNumber) {
-    console.log("Loading question number: " + qNumber);
-    question.innerText = christmasQuestions[qNumber].question;
+    if (question) {
+        question.innerText = christmasQuestions[qNumber].question;
+    }
     answer1.removeAttribute('disabled');
     answer2.removeAttribute('disabled');
     answer3.removeAttribute('disabled');
     answer4.removeAttribute('disabled');
     answer5.removeAttribute('disabled');
 }
+
 /**
- * function to load the answer to the question
+ * Function to load the answers to the question
  */
 function loadGameAnswer(qNumber) {
-    answer1.innerText = christmasQuestions[qNumber].answers[0];
-    answer2.innerText = christmasQuestions[qNumber].answers[1];
-    answer3.innerText = christmasQuestions[qNumber].answers[2];
-    answer4.innerText = christmasQuestions[qNumber].answers[3];
-    answer5.innerText = christmasQuestions[qNumber].answers[4];
+    if (answer1) answer1.innerText = christmasQuestions[qNumber].answers[0];
+    if (answer2) answer2.innerText = christmasQuestions[qNumber].answers[1];
+    if (answer3) answer3.innerText = christmasQuestions[qNumber].answers[2];
+    if (answer4) answer4.innerText = christmasQuestions[qNumber].answers[3];
+    if (answer5) answer5.innerText = christmasQuestions[qNumber].answers[4];
 }
+
 /**
- * checks what the user clicked is the correct answer
+ * Checks if the user's answer is correct
  */
 function check(ansnum) {
-    // check if the answer is correct
-    let correct = christmasQuestions[qNumber].correct;
-    if (ansnum === correct) {
-        // increment score
-        scoreNumber++;
-        score.innerHTML = scoreNumber;
-    }
-    // increment the question number
-    qNumber++;
-    if (qNumber === qlength) {
-        // makes visible the play again button
-        endGameFunc();
-    } else {
-        loadGameQuestion(qNumber);
-        loadGameAnswer(qNumber);
+    if (qNumber < christmasQuestions.length) {
+        let correct = christmasQuestions[qNumber].correct;
+        if (ansnum === correct) {
+            scoreNumber++;
+            if (score) score.innerHTML = scoreNumber;
+        }
+
+        // Increment the question number
+        qNumber++;
+
+        console.log("Current question number:", qNumber); // Debug log
+        console.log("Total questions length:", qlength);  // Debug log
+
+        if (qNumber === qlength) {
+            // End the game if it's the last question
+            console.log("End game function called."); // Debug log
+            endGameFunc();
+        } else {
+            // Load the next question
+            loadGameQuestion(qNumber);
+            loadGameAnswer(qNumber);
+        }
     }
 }
+
 /**
- * makes the play again section visible
-*/
+ * Makes the play again section visible
+ */
 function endGameFunc() {
-    heroText.innerHTML = `
-    <h1>Would you like to play again?</h1>
-    <button onclick="endOption(0)">Yes</button>
-    <button onclick="endOption(1)">No</button>`;
+    console.log("Inside endGameFunc."); // Debug log
 
-    // Make the play-Again section visible by including it in the heroText container
-    if (again !== null) {
-        again.parentNode.innerHTML += `<button id="play-Again">Play Again</button>`;
+    // Clear the question text
+    if (question) question.innerText = '';
+
+    // Hide the answers
+    if (answer1) answer1.style.display = 'none';
+    if (answer2) answer2.style.display = 'none';
+    if (answer3) answer3.style.display = 'none';
+    if (answer4) answer4.style.display = 'none';
+    if (answer5) answer5.style.display = 'none';
+
+    // Update heroText with play-again options
+    if (heroText) {
+        console.log("Updating heroText with play-again options."); // Debug log
+        heroText.innerHTML = "Would you like to play again?";
+
+        // Create Yes button
+        let yesButton = document.createElement("button");
+        yesButton.innerText = "Yes";
+        yesButton.onclick = function() { endOption(0); };
+        heroText.appendChild(yesButton);
+
+        // Create No button
+        let noButton = document.createElement("button");
+        noButton.innerText = "No";
+        noButton.onclick = function() { endOption(1); };
+        heroText.appendChild(noButton);
+    } else {
+        console.error("heroText element not found."); // Debug log
     }
 }
 
 /**
- * function to end the game
+ * Function to end the game
  */
 function endOption(chosenOpt) {
     if (chosenOpt === 0) {
         window.location.reload();
     } else {
-      heroText.innerHTML = `
-      <h1>Thanks for playing</h1>`
+        if (heroText) heroText.innerText = "Thanks for playing";
     }
 }
+
 /**
- * function to start the game
+ * Function to start the game
  */
 document.getElementById("startQuizBtn").addEventListener("click", startchristmasQuiz);
 function startchristmasQuiz() {
-    // hides start quiz button
-    document.getElementById("startQuizBtn").style.display = "none";
-    // hides the play again button
-    if (again !== null) {
-        again.style.visibility = "hidden";
-    }
-    document.getElementById("answerOne").style.display = 'inline-block';
-    document.getElementById("answerTwo").style.display = 'inline-block';
-    document.getElementById("answerThree").style.display = 'inline-block';
-    document.getElementById("answerFour").style.display = 'inline-block';
-    document.getElementById("answerFive").style.display = 'inline-block';
+    // Hide start quiz button
+    if (startquiz) startquiz.style.display = "none";
+
+    // Show the answer buttons
+    if (answer1) answer1.style.display = 'inline-block';
+    if (answer2) answer2.style.display = 'inline-block';
+    if (answer3) answer3.style.display = 'inline-block';
+    if (answer4) answer4.style.display = 'inline-block';
+    if (answer5) answer5.style.display = 'inline-block';
+
     // Initialize the game
     qNumber = 0;
     scoreNumber = 0;
-    // loads game questions and answers
+
+    // Load game questions and answers
     loadGameQuestion(qNumber);
     loadGameAnswer(qNumber);
+
+    // Show the modal
     showModal();
 }
+
 /**
  * Function to show the modal and start the quiz
  */
 function showModal() {
-    modal.style.display = "block";
-    document.getElementById("heroText").style.display = "visible"; // Show the hero-text
+    if (modal) modal.style.display = "block";
+    if (heroText) heroText.style.display = "block"; // Show the hero-text
 }
+
+// Add debug logs to trace function execution
+console.log("Script loaded and ready.");
